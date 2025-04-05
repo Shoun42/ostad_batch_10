1223b9fe3d30f103354d0a5fd2d1fd24deeefa00
Shoun42 <shounbabu31@gmail.com>
2025-04-05 20:34:39 +0600

    Initial commit

---
+++ bin/vehicle_assignment.dart
@@ -0,0 +1,20 @@
+
+abstract class Vehicle {
+  int _speed = 0;
+  void  move();
+  void setSpeed(int speed) {
+    _speed = speed;
+  }
+  int get speed => _speed;
+}
+class Car extends Vehicle {
+  @override
+  void move() {
+    print('The car is moving at $speed Km/h');
+  }
+}
+void main() {
+  Car myCar = Car();
+  myCar.setSpeed(120);
+  myCar.move();
+}
